b69b17d581f9889287838b9d9768e6ae1a7a0873
Princekumar1999 <61105671+Princekumar1999@users.noreply.github.com>
2020-06-17 13:58:31 +0530

    Add files via upload

---
+++ file_code.py
@@ -0,0 +1,13 @@
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+ff = fh.read()
+mino = 0.0
+count = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : continue
+    count = count+1
+    lino = line.find(':')
+    kino = float(line[lino+1:])
+    mino = kino + mino
+print('Average spam confidence:',mino/count)
